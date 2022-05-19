fd6c030b092a9a1b15cf8e6cec58dc3a175e141d
fitransyah-sourcetree <fitransyah.rusman@gmail.com>
2022-05-19 18:54:33 +0700

    apply body parser

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
 
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ encoded : true }));
+
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.use(express.static("public"));
+app.post("/", function(req,res){
+  console.log(req.body.firstName);
+
+  res.send("submitted");
+});
 
 app.listen(3000, function(){
   console.log("Server 3000 ready nih!")
