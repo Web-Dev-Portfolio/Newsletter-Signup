a4944f04090464bb7b7f97ecffd8ecd577128ae1
fitransyah-sourcetree <fitransyah.rusman@gmail.com>
2022-05-21 01:33:33 +0700

    testing API key with https req

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
+const https = require("https");
 
 
 
@@ -13,11 +14,28 @@ app.get("/", function(req,res){
 });
 
 app.post("/", function(req,res){
-  console.log(req.body.firstName);
 
-  res.send("submitted");
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  console.log(firstName,lastName,email);
+
+  const url = "https://us8.api.mailchimp.com/3.0/lists/e00fc1594e";
+  const options = {
+    method : "POST",
+    auth : "fitransyah:5feed6297af57a8f01cb92d5bc1de4ec-us8"
+  };
+  const request = https.request(url, options,function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+  request.end();
+  //res.send("submitted");
 });
 
 app.listen(3000, function(){
   console.log("Server 3000 ready nih!")
 });
+
+// endpoint : https://usX.api.mailchimp.com/3.0/lists/{list_id}
