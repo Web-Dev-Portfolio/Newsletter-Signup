aed4daa521477d6ba50ec0653d34d5feb310d540
fitransyah-sourcetree <fitransyah.rusman@gmail.com>
2022-05-21 01:57:48 +0700

    testing with data from sign up page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
-const https = require("https");
+const https = require("https")
 
 
 
@@ -11,31 +11,46 @@ app.use(bodyParser.urlencoded({ encoded : true }));
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/signup.html");
-});
+})
 
 app.post("/", function(req,res){
 
   const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
+  const lastName = req.body.lastName
+  const email = req.body.email
   console.log(firstName,lastName,email);
+  const members = {
+    members : [
+      {
+        email_address : email,
+        status : "subscribed",
+        merge_fields : {
+          FNAME : firstName,
+          LNAME : lastName,
+          EMAIL : email
+        }
+    }
+  ]
+  }
+  const jsonData = JSON.stringify(members);
 
   const url = "https://us8.api.mailchimp.com/3.0/lists/e00fc1594e";
   const options = {
     method : "POST",
-    auth : "fitransyah:5feed6297af57a8f01cb92d5bc1de4ec-us8"
-  };
+    auth : "fitransyah:myAPIkey"
+  }
   const request = https.request(url, options,function(response){
     response.on("data", function(data){
       console.log(JSON.parse(data));
-    });
-  });
-  request.end();
+    })
+  })
+  request.write(jsonData)
+  request.end()
   //res.send("submitted");
-});
+})
 
 app.listen(3000, function(){
   console.log("Server 3000 ready nih!")
-});
+})
 
 // endpoint : https://usX.api.mailchimp.com/3.0/lists/{list_id}
